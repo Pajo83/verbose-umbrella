93aef739f1e8194a20f44938211ebc7fe06304fc
Pavle Buzaroski <pavlebuzaroski@outlook.com>
2020-01-20 20:45:07 +0100

    a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("HO, Ho, HO");
+            Console.WriteLine("F2");
 
         }
     }
